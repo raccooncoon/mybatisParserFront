165811bb2ca012fa10786a942e39d7f2be237e92
임민영 <raccoon@shinsegae.com>
2023-11-24 03:04:29 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import XmlListView from "./XmlListView";
+import CssBaseline from '@mui/material/CssBaseline';
+import {Fragment} from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Fragment>
+          <CssBaseline />
+          <XmlListView/>
+      </Fragment>
+
   );
 }
 
---
+++ src/XmlListView.jsx
@@ -0,0 +1,183 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Container from '@mui/material/Container';
+import {TextField} from "@mui/material";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Chip from '@mui/material/Chip';
+
+
+const XmlListView = () => {
+    const [data, setData] = useState([]);
+    const [pageInfo, setPageInfo] = useState({last: false, pageSize: 20, pageNumber: 0, totalElements: 0});
+    const [searchTerm, setSearchTerm] = useState('');
+    const [mapperTypes, setMapperTypes] = useState(['update', 'delete', 'insert']);
+
+    const defaultPageInfo = { pageSize: 20, pageNumber: 0, last: false, totalElements: 0 };
+
+    useEffect(() => {
+        fetchData();
+    }, [searchTerm, mapperTypes]); // Re-fetch data when searchTerm changes
+
+    const fetchData = async () => {
+        console.log(">>>>>>>>>>>>>>.");
+        console.log(mapperTypes);
+        console.log(pageInfo);
+        try {
+            if(searchTerm.length > 1) {
+                const response = await axios.get(`http://localhost:8080/mapperBody/${searchTerm}`, {
+                    params: {
+                        page: pageInfo.pageNumber,
+                        size: pageInfo.pageSize,
+                        mapperTypes: mapperTypes.toString(),
+                    },
+                });
+                setData(response.data.content);
+                setPageInfo({
+                    pageSize: response.data.pageable.pageSize,
+                    pageNumber: response.data.pageable.pageNumber,
+                    last: response.data.last,
+                    totalElements: response.data.totalElements,
+                });
+            } else {
+                setData([]);
+                setPageInfo(defaultPageInfo);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleChange = (event) => {
+        const {id, checked} = event.target;
+
+        setMapperTypes((prevTypes) => {
+            if (checked) {
+                return [...prevTypes, id];
+            } else {
+                return prevTypes.filter((item) => item !== id);
+            }
+        });
+
+        // 페이징 초기값 업데이트
+        setPageInfo(defaultPageInfo);
+        setData([]);
+    };
+
+    const fetchMoreData = async () => {
+        // 페이징 정보 업데이트
+        try {
+            if (!pageInfo.last) {
+                const response = await axios.get(`http://localhost:8080/mapperBody/${searchTerm}`, {
+                    params: {
+                        page: pageInfo.pageNumber + 1, // Increment the page number
+                        size: pageInfo.pageSize,
+                        mapperTypes: mapperTypes.toString(),
+                    },
+                });
+
+                setData((prevData) => [...prevData, ...response.data.content]);
+                // Update the pageInfo
+                setPageInfo({
+                    pageSize: response.data.pageable.pageSize,
+                    pageNumber: response.data.pageable.pageNumber,
+                    last: response.data.last,
+                    totalElements: response.data.totalElements,
+                });
+            }
+        } catch (error) {
+            console.error('Error fetching more data:', error);
+        }
+    }
+
+
+    return (
+        <Container fixed style={{ height: '100vh'}}>
+            <Grid container spacing={2} justifyContent={"space-between"}>
+                <Grid item xs={4}>
+                    <FormControlLabel
+                        label="C"
+                        control={<Checkbox id="insert" defaultChecked color="secondary" onChange={handleChange}/>}
+                    />
+                    <FormControlLabel
+                        label="R"
+                        control={<Checkbox id="select" color="default" onChange={handleChange}/>}
+                    />
+                    <FormControlLabel
+                        label="U"
+                        control={<Checkbox id="update" defaultChecked onChange={handleChange}/>}
+                    />
+                    <FormControlLabel
+                        label="D"
+                        control={<Checkbox id="delete" defaultChecked color="success" onChange={handleChange}/>}
+                    />
+                    <Chip label={pageInfo.totalElements}  />
+                </Grid>
+                <Grid item xs={3}>
+                    <TextField
+                        id="searchField"
+                        label="테이블 이름 입력"
+                        type="search"
+                        variant="standard"
+                        fullWidth
+                        onChange={handleSearch}/>
+                </Grid>
+            </Grid>
+            <InfiniteScroll
+                dataLength={data.length} // 현재 표시된 아이템 수
+                next={fetchMoreData} // 스크롤이 바닥에 닿을 때 호출되는 함수
+                hasMore={!pageInfo.last} // 더 많은 아이템이 있는지 여부
+                //loader={<Typography>...</Typography>} // 로딩 중에 보여질 컴포넌트
+            >
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>No</TableCell>
+                            {/*<TableCell>ID</TableCell>*/}
+                            <TableCell>SERVICE_NAME</TableCell>
+                            <TableCell>MAPPER_TYPE</TableCell>
+                            <TableCell>MAPPER_ID</TableCell>
+                            <TableCell>MAPPER_NAME</TableCell>
+                            <TableCell>FILE_NAME</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((row,index) => (
+                            <TableRow
+                                key={row.id}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {index + 1}
+                                </TableCell>
+                                {/*<TableCell>{row.id}</TableCell>*/}
+                                <TableCell>{row.serviceName}</TableCell>
+                                <TableCell>{row.mapperType}</TableCell>
+                                <TableCell>{row.mapperId}</TableCell>
+                                <TableCell>{row.mapperName}</TableCell>
+                                <TableCell>{row.fileName}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+          </InfiniteScroll>
+        </Container>
+    )
+};
+
+export default XmlListView;
